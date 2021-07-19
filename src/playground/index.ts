563ba4bb4d200859547d0bf2418794f426f36aa6
LeGrosBubu <legrosbubu@gmail.com>
2021-07-19 11:56:09 +0200

    restore playground

--- src/playground/index.ts
+++ src/playground/index.ts
@@ -19,12 +19,39 @@ appInsights.setup('7d1aecfe-f159-4a69-ac00-12d6436453de')
 
 const appInsightsClient = appInsights.defaultClient;
 
-const app = express();
+import * as dgram from 'dgram';
+import {constants, F1TelemetryClient} from '..';
 
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('Hello World!');
+const {PACKETS} = constants;
+
+const client = new F1TelemetryClient({
+  port: 20777,
+  forwardAddresses: [{port: 5550}],
+  skipParsing: true,
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+const socket = dgram.createSocket('udp4');
+socket.bind(5550);
+
+socket.on('message', (msg) => {
+  const parsedmsg = F1TelemetryClient.parseBufferMessage(msg);
+
+  if(parsedmsg?.packetData?.data===undefined){
+  } else {
+    console.clear();
+  console.log(parsedmsg?.packetData?.data);
+  }
+});
+
+client.start();
+
+// stops the client
+[`exit`,
+ `SIGINT`,
+ `SIGUSR1`,
+ `SIGUSR2`,
+ `uncaughtException`,
+ `SIGTERM`,
+].forEach((eventType) => {
+  (process as NodeJS.EventEmitter).on(eventType, () => client.stop());
 });
