fe2a82e03a4d42eee1c518ebe878b872064bc9e6
Astymeus <live@astyweb.com>
2021-07-19 13:18:24 +0200

    Update index.ts
    Configure for VPS

--- src/playground/index.ts
+++ src/playground/index.ts
@@ -2,37 +2,20 @@ import * as dgram from 'dgram';
 
 import {constants, F1TelemetryClient} from '..';
 
-const port = Number(process.env.PORT || 20777);
+const address = '0.0.0.0';
+const port = 20777;
 
 const {PACKETS} = constants;
 
-const appInsights = require('applicationinsights');
-
-appInsights.setup('7d1aecfe-f159-4a69-ac00-12d6436453de')
-    .setAutoDependencyCorrelation(true)
-    .setAutoCollectRequests(true)
-    .setAutoCollectPerformance(true, true)
-    .setAutoCollectExceptions(true)
-    .setAutoCollectDependencies(true)
-    .setAutoCollectConsole(true)
-    .setUseDiskRetryCaching(true)
-    .setSendLiveMetrics(false)
-    .setDistributedTracingMode(appInsights.DistributedTracingModes.AI)
-    .start();
-
-const appInsightsClient = appInsights.defaultClient;
-
 const client = new F1TelemetryClient({
+  address,
   port,
   skipParsing: true,
 });
 
-appInsightsClient.trackTrace({message: 'Démarrage du client'});
-
 function eventLog(eventName: string) {
   return function eventLogWithMsg(msg: string) {
     console.log(eventName, msg);
-    appInsightsClient.trackTrace({message: eventName + ': ' + msg});
   };
 }
 
