099d7ea94e8ab31b1d5a52a104a1051a6e60ed46
Astymeus <live@astyweb.com>
2021-07-19 14:17:55 +0200

    Fix size of object

--- src/playground/index.ts
+++ src/playground/index.ts
@@ -15,8 +15,51 @@ const client = new F1TelemetryClient({
 let last = new Date();
 let sumData = 0;
 
-function roughSizeOfObject(data: object) {
-  return Object.keys(data).length;
+function memorySizeOf(obj: object) {
+  let bytes = 0;
+
+  function sizeOf(obj: object) {
+    if (obj !== null && obj !== undefined) {
+      switch (typeof obj) {
+        case 'number':
+          bytes += 8;
+          break;
+        case 'string':
+          bytes += obj.length * 2;
+          break;
+        case 'boolean':
+          bytes += 4;
+          break;
+        case 'object':
+          const objClass = Object.prototype.toString.call(obj).slice(8, -1);
+          if (objClass === 'Object' || objClass === 'Array') {
+            for (const key in obj) {
+              if (!obj.hasOwnProperty(key)) continue;
+              sizeOf(obj[key]);
+            }
+          } else
+            bytes += obj.toString().length * 2;
+          break;
+        default:
+          break;
+      }
+    }
+    return bytes;
+  }
+
+
+  return sizeOf(obj);
+}
+
+function formatByteSize(bytes: number) {
+  if (bytes < 1024)
+    return bytes + ' bytes';
+  else if (bytes < 1048576)
+    return (bytes / 1024).toFixed(3) + ' KiB';
+  else if (bytes < 1073741824)
+    return (bytes / 1048576).toFixed(3) + ' MiB';
+  else
+    return (bytes / 1073741824).toFixed(3) + ' GiB';
 }
 
 function eventLog(eventName: string) {
@@ -24,11 +67,13 @@ function eventLog(eventName: string) {
     const now = new Date();
     if (now.getTime() - last.getTime() > ELAPSE_TIME) {
       last = now;
-      console.info('data receive in last ', ELAPSE_TIME, 'seconds: ', sumData);
+      console.info(
+          'data received in last', ELAPSE_TIME,
+          'seconds:', formatByteSize(sumData));
       sumData = 0;
     }
 
-    sumData += roughSizeOfObject(msg);
+    sumData += memorySizeOf(msg);
 
     console.debug(eventName, ': ', msg);
   };
